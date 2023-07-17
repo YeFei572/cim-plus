3a37047f95eee736cd7a937c15d2d9dd7862eb0f
crazyBB <572yefei@163.com>
2023-07-17 18:29:44 +0800

    Create TlogConfiguration.java

---
+++ cim-server/src/main/java/cn/v2ss/server/config/TlogConfiguration.java
@@ -0,0 +1,12 @@
+package cn.v2ss.server.config;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TlogDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TlogDemoApplication.class, args);
+    }
+
+}
